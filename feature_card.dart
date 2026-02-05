ed26b99dbbccb8ab7534058b12d034ee083592ca
SHOBANA-PM <shobz2006@gmail.com>
2026-02-05 10:26:37 +0530

    Add FeatureCard widget for displaying features

---
+++ feature_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class FeatureCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const FeatureCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: Icon(icon, size: 40),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(subtitle),
+        trailing: const Icon(Icons.arrow_forward_ios),
+        onTap: onTap,
+      ),
+    );
+  }
+}
